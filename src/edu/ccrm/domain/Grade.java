b3a414c6aa7fa27b4ee669bce8bd4cad299c6bc2
Azahar_Mahmud <chowdhuryrafi0211@gmail.com>
2026-04-07 11:33:19 +0600

    Update Grade.java

--- src/edu/ccrm/domain/Grade.java
+++ src/edu/ccrm/domain/Grade.java
@@ -15,14 +15,4 @@ public enum Grade {
     public double getGradePoint() {
         return gradePoint;
     }
-
-    public static Grade fromMarks(int marks) {
-        if (marks >= 90) return S;
-        if (marks >= 80) return A;
-        if (marks >= 70) return B;
-        if (marks >= 60) return C;
-        if (marks >= 50) return D;
-        if (marks >= 40) return E;
-        return F;
-    }
 }
